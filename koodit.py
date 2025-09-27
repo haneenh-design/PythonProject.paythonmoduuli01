b31aa3d7b77f8afc1884f88da7a7d51d419fcbba
haneenh-design <haneenh@metropolia.fi>
2025-09-27 11:28:45 +0300

    Add files via upload

---
+++ koodit.py
@@ -0,0 +1,10 @@
+import random
+
+# Kolminumeroinen koodi (0–9)
+koodi1 = "".join(str(random.randint(0, 9)) for _ in range(3))
+
+# Nelinumeroinen koodi (1–6)
+koodi2 = "".join(str(random.randint(1, 6)) for _ in range(4))
+
+print(f"Kolminumeroinen koodi: {koodi1}")
+print(f"Nelinumeroinen koodi: {koodi2}")
