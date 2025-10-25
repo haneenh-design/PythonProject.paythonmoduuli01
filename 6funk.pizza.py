e1d4955344d49ce33553405abe66c15d6a167466
Haneen Hasan <haneenh@metropolia.fi>
2025-10-25 15:00:41 +0300

    Tallennettu muutokset ennen haaran vaihtoa

--- 6funk.pizza.py
+++ 6funk.pizza.py
@@ -3,7 +3,7 @@ import math
 def yksikkohinta(halk_cm, hinta):
     """Laske pizzan yksikköhinnan €/m^2."""
     sade = halk_cm / 2
-    pinta_ala = 3.14 * sade ** 2 / 10000
+    pinta_ala_m^2 = 3.14 * sade * sade / 1000 # neliömetreiksi
     return hinta / pinta_ala
 
 # Pääohjelma
@@ -12,9 +12,6 @@ hinta1 = float(input("Anna 1. pizzan hinta:"))
 
 halk2= float(input("Anna 2. pizzan halkaisija (cm)"))
 hinta2 = float(input("Anna 2. pizzan hinta:"))
-
-yks1 = yksikkohinta(halk1, hinta1)
-yks2 = yksikkohinta(halk2, hinta2)
 
 print(f"1. pizzan yksikköhinta: {yks1:.2f} €/m²")
 print(f"2. pizzan yksikköhinta: {yks2:.2f} €/m²")
